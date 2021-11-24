3a859fe4d77bed795c1ea9d23222cd50fa20468a
LukeGotBored <gianluca.suriani@outlook.it>
2021-11-24 12:35:13 +0100

    Inizializzazione del Branch + Interfaccia iniziale

--- app/src/main/java/mlitts/voltapg/esegitteam5b/MainActivity.java
+++ app/src/main/java/mlitts/voltapg/esegitteam5b/MainActivity.java
@@ -1,14 +1,34 @@
 package mlitts.voltapg.esegitteam5b;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    Button mainButton;
 
+    ConstraintLayout layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainButton = findViewById(R.id.mainbutton);
+        layout = findViewById(R.id.mainlayout);
+
     }
+
+    public void showText(View v){
+        Toast.makeText(this, "Perché l'hai fatto?", Toast.LENGTH_SHORT).show();
+
+
+    }
+
 }
