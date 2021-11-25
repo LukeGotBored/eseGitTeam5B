3cc6591de70e66d6d091ec08665484f6ebc00360
biar22 <fra.biarella03@gmail.com>
2021-11-25 19:42:12 +0100

    modifiche Biarella

--- app/src/main/java/mlitts/voltapg/esegitteam5b/MainActivity.java
+++ app/src/main/java/mlitts/voltapg/esegitteam5b/MainActivity.java
@@ -3,6 +3,8 @@ package mlitts.voltapg.esegitteam5b;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(), R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
